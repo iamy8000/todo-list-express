5928dd45273c22651f1ca644aabf3cb3f45d706e
iamy8000 <amy8000@gmail.com>
2020-11-05 17:48:47 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 
 const routes = require('./routes')
 
@@ -24,10 +25,14 @@ app.use(methodOverride('_method'))
 
 usePassport(app)
 
+app.use(flash())
 app.use((req, res, next) => {
   //一登入狀態切換導覽列：登入或登出
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  //提示訊息
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -19,12 +19,36 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const erros = []
+  //表單填寫錯誤：
+  if (!name || !email || !password || !confirmPassword) {
+    erros.push({ message: '所有欄位都是必填。' })
+  }
+  if (password !== confirmPassword) {
+    erros.push({ message: '密碼與確認密碼不相符！' })
+  }
+  if (erros.length) {
+    return res.render('register', {
+      erros,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
+
   //檢查使用者是否已經存在
   User.findOne({ email }).then(user => {
     //使用者已存在：退回註冊頁面
     if (user) {
-      console.log('user already existed!')
-      res.render('register', { name, email, password, confirmPassword })
+      erros.push({ message: '這個email已經註冊過了。' })
+      res.render('register', {
+        erros,
+        name,
+        email,
+        password,
+        confirmPassword
+      })
     } else {
       //建立新的使用者
       return User.create({
@@ -40,6 +64,7 @@ router.post('/register', (req, res) => {
 
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '你已經成功登出。')
   res.redirect('/users/login')
 })
 
